3f992e6ac65c93fa7267331638d1baed88b24e4b
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-08-09 23:16:44 +0700

    save

--- wp-content/themes/theme-custom/tin-tuc.php
+++ wp-content/themes/theme-custom/tin-tuc.php
@@ -612,8 +612,7 @@ include 'inc/header.php';
 
                                 while ($query->have_posts()):
                                     $query->the_post();
-
-                                    my_print($query->the_post());
+                                    echo the_post_thumbnail_url()."/têttteetetetee";
                                     // Lấy ngày tạo bài viết
                                     $post_date = get_the_date('Y-m-d H:i:s', get_the_ID());
                                     $post_timestamp = strtotime($post_date);
