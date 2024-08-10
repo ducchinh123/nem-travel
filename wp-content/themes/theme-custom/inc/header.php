65a030d645fdfe3fdb49ac6e826cf6e589a703eb
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-08-10 10:00:35 +0700

    save

--- wp-content/themes/theme-custom/inc/header.php
+++ wp-content/themes/theme-custom/inc/header.php
@@ -20,7 +20,7 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="<?= site_url() ?>">Khách sạn <span class="sr-only">(current)</span></a></li>
+          <li class=""><a href="<?= site_url() ?>">Khách sạn <span class="sr-only">(current)</span></a></li>
           <li><a href="<?= site_url() . "/tour" ?>">Tours</a></li>
           <li><a href="<?= site_url() . "/blog" ?>">Tin Tức</a></li>
           <li><a href="">Liên hệ</a></li>
