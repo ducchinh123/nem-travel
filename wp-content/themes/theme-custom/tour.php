597ee5230c6546c7b9e89799ef33d2c7ca6d9212
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-08-10 09:10:03 +0700

    save

--- wp-content/themes/theme-custom/tour.php
+++ wp-content/themes/theme-custom/tour.php
@@ -18229,7 +18229,7 @@
         <div class="content-wrapper" style="min-height: 267px;">
 
             <!-- form search -->
-            <?php include 'inc/form_tour.php' ?>
+         
             <!-- end form search -->
 
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 whyIvivu no-padding hidden-xs">
